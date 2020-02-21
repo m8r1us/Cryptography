1bf112e426f8abd3261f9a65103d9076d4a1913b
Marius Elmiger <marius@elmigers.ch>
2020-02-21 09:18:20 +0100

    v0.5

--- wk04/my-rsa-calculator.py
+++ wk04/my-rsa-calculator.py
@@ -1,4 +1,6 @@
-# V0.4
+# V0.5
+# Author: M8r1us
+# Link: https://github.com/m8r1us/esecurity
 
 import binascii
 import random
@@ -9,17 +11,7 @@ import math
 # -----------------
 
 e = int(65537) # A very common value for the public exponent is e=65537, because 65537=216+1, which has a nice binary representation (like all Fermat primes): 10000000000000001
-length = 28
-check_p = False
-check_q = False
-'''
-msg = int(18)
-p = int(17)
-q = int(29)
-e = int(3)
-n = p*q
-phi = (p-1)*(q-1)
-'''
+length = 29
 
 # -----------------
 # 1 FUNCTIONS
@@ -86,31 +78,6 @@ def gcd(a, b):
         print ("a:", a)
     return a
 
-# Encryption
-def encrypt(pub_key,n_text):
-    e,n=pub_key
-    hex_data   = binascii.hexlify(n_text.encode())
-    plain_text = int(hex_data, 16)
-    print('plain text integer      ', plain_text)
- 
-    if plain_text > n:
-        raise Exception('plain text too large for key')
- 
-    encrypted_text = pow(plain_text, e, n)
-    #encrypted_text = (plain_text**e)%n
-    #print('encrypted text integer  ', encrypted_text)
-    return encrypted_text
-
-# Decryption
-def decrypt(priv_key,c_text):
-    d,n=priv_key
-    decrypted_text = pow(c_text, d, n)
-    #decrypted_text=(int(c_text)**d)%n
-    print('decrypted text integer  ', decrypted_text)
-    decrypted_decoded = binascii.unhexlify(hex(decrypted_text)[2:]).decode() # [2:] slicing, to strip the 0x part 
-    #print('message                 ', decrypted_decoded)     
-    return decrypted_decoded
-
 #Extended Euclidean Algorithm
 def eea(a,b):
     if(a%b==0):
@@ -122,6 +89,13 @@ def eea(a,b):
         return(gcd,t,s)
  
 #Multiplicative Inverse
+''' The following code would also calculate d. But it's slower
+for d in range(3, phi, 2):
+    if d * e % phi == 1:
+        break
+else:
+    raise AssertionError("cannot find 'd' with p={!r}, q={!r} and e={!r}".format(p, q, e))
+'''
 def mult_inv(e,r):
     gcd,s,_=eea(e,r)
     if(gcd!=1):
@@ -132,15 +106,37 @@ def mult_inv(e,r):
         elif(s>0):
             print("s=%d."%(s))
         return s%r
+
+# Encryption
+def encrypt(pub_key,n_text):
+    e,n=pub_key
+    hex_data = binascii.hexlify(n_text.encode())
+    plain_text = int(hex_data, 16)
+    print("plain text integer: \t\t", plain_text)
+
+    if plain_text > n:
+        raise Exception("plain text too large for key (n)")
+
+    encrypted_text = pow(plain_text, e, n) #encrypted_text = (plain_text**e)%n
+    
+    return encrypted_text
+
+# Decryption
+def decrypt(priv_key,c_text):
+    d,n=priv_key
+    decrypted_text = pow(c_text, d, n) #decrypted_text=(int(c_text)**d)%n
+    print("decrypted text integer: \t", decrypted_text)
+    decrypted_decoded = binascii.unhexlify(hex(decrypted_text)[2:]).decode() # [2:] slicing, to strip the 0x part    
+
+    return decrypted_decoded
+
 # --------- MAIN ----------
+print("-------------------------------------------------------")
+print("-> 1 STEP <-")
+print("Create a public-private key pair (p and q)")
+print("-------------------------------------------------------\n")
 RandomGenerator = input("RandomGenerator for the Primes? (Yes/No): ")
 
-# Generate Key Pair
-"""Create a public-private key pair.
-The key pair is generated from two random prime numbers. 
-"""
-
-# -> 1 STEP <-
 # Define p and q
 if RandomGenerator == "Yes":
     if length < 4:
@@ -175,6 +171,8 @@ if RandomGenerator == "Yes":
         raise AssertionError("cannot find 'p' and 'q' for a key of length={!r}".format(length))
 # ELSE: Prime Wizard
 else:
+    check_p = False
+    check_q = False
     while(((check_p==False)or(check_q==False))):
         p = int(input("Enter a prime number for p (17): "))
         q = int(input("Enter a prime number for q (29): "))
@@ -185,69 +183,103 @@ else:
 print("The first factor of the modulus (p) is: ",p)
 print("The second factor of the modulus (q) is: ",q)
 
-# Calculate 'n'
+print("\n-------------------------------------------------------")
+print("-> 2 STEP <-")
+print("Compute n = p*q")
+print("-------------------------------------------------------\n")
+
 # Compute n = pq. n is used as the modulus for both the public and private keys. Its length is the key length.
 n = p * q
-print("RSA Modulus(n) is: ",n)
+print("n is: ",n)
+
+print("\n-------------------------------------------------------")
+print("-> 3 STEP <-")
+print("Compute phi = (p-1)*(q-1)")
+print("-------------------------------------------------------\n")
 
 #Eulers Toitent 'phi'
 # Compute φ(n) = φ(p)φ(q) = (p − 1)(q − 1) where φ is Euler’s totient function. (https://en.wikipedia.org/wiki/Euler%27s_totient_function)
 phi= (p-1)*(q-1)
 print("Eulers Toitent(phi) is: ",phi)
 
-# -> 2 STEP <-
+print("\n-------------------------------------------------------")
+print("-> 4 STEP <-")
+print("Choose an integer e such that 1 < e < phi and gcd(e, phi)")
+print("-------------------------------------------------------\n")
+
 # -> e Value Calculation
 # Choose an integer e such that 1 < e < phi and gcd(e, phi) = 1; i.e., e and n are coprime. e is released as the public key exponent.
-# choose a number ``e`` lower than ``(p - 1) * (q - 1)``
-# which shares no factors with ``(p - 1) * (q - 1)``.
+# choose a number ``e`` lower than ``(p - 1) * (q - 1)`` which shares no factors with ``(p - 1) * (q - 1)``.
 
 if phi < e:
+    e = int(3) # Take 3 as e otherwise 65537
+
+    # The following method would calculate other e
+    '''
     for e in range(3, phi, 2):
         if are_relatively_prime(e, phi):
             break
         else:
             AssertionError("cannot find 'e' with p={!r} and q={!r}".format(p, q))
-else:
-    # assign 65537
-    e = e
-
+    '''
 print("The value of e is: ",e)
-print("-------------------------------------------------------")
 
-# -> 3 STEP <-
+print("\n-------------------------------------------------------")
+print("-> 5 STEP <-")
+print("Find d such that (d * e - 1) is divisible by phi (inverse modulus)")
+print("-------------------------------------------------------\n")
+
 # -> d Value Calculation
 # Third step: find ``d`` such that ``(d * e - 1)`` is divisible by ``(p - 1) * (q - 1)``.
 
 d = mult_inv(e,phi)
-''' also calculated d but slower
-for d in range(3, phi, 2):
-    if d * e % phi == 1:
-        break
-else:
-    raise AssertionError("cannot find 'd' with p={!r}, q={!r} and e={!r}".format(p, q, e))
-'''
-print("END OF THE STEPS USED TO ACHIEVE THE VALUE OF 'd'.")
 print("The value of d is: ",d)
-print("-------------------------------------------------------")
-public = (e,n)
+
+print("\n-------------------------------------------------------")
+print("-> 6 STEP <-")
+print("Build private (d,n) and public (e,n) key")
+print("-------------------------------------------------------\n")
+
+# Private and public key
 private = (d,n)
+public = (e,n)
+
 print("Private Key is: ",private)
 print("Public Key is: ",public)
+
+print("\n-------------------------------------------------------")
+print("-> SUMMARY <-")
 print("-------------------------------------------------------")
 
+print("p is: \t\t",p)
+print("q is: \t\t",q)
+print("n is: \t\t",n)
+print("phi is: \t",phi)
+print("e is: \t\t",e)
+print("d is: \t\t",d)
+print("Private Key is: ",private)
+print("Public Key is: \t",public)
+
+print("\n-------------------------------------------------------")
+print("-> 7 STEP <-")
+print("Message encryption / decryption")
+print("-------------------------------------------------------\n")
+
 #Message
-#message = input("What would you like encrypted or decrypted?(Separate numbers with ',' for decryption):")
-message = input("What would you like encrypted or decrypted? (e.g. red):")
-print("Your message is: ",message)
-print("-------------------------------------------------------")
+message = input("What would you like encrypted or decrypted? (e.g. red / 82616517): ")
+print("Your input is: ",message)
+print("-------------------------------------------------------\n")
 #Choose Encrypt or Decrypt and Print
-choose = input("Type '1' for encryption and '2' for decryption.")
+choose = input("Type '1' for encryption and '2' for decryption: ")
+print("-------------------------------------------------------")
 if(choose=='1'):
     enc_msg=encrypt(public,message)
-    print("Your encrypted message is: ",enc_msg)
+    print("Your encrypted message is: \t",enc_msg)
     print("-------------------------------------------------------")
-    print("Your decrypted message is: ",decrypt(private,enc_msg))
+    print("Your decrypted message is: \t",decrypt(private,enc_msg))
+    print("-------------------------------------------------------")
 elif(choose=='2'):
-    print("Your decrypted message is: ",decrypt(private,int(message)))
+    print("Your decrypted message is: \t",decrypt(private,int(message)))
+    print("-------------------------------------------------------")
 else:
     print("You entered the wrong option.")
