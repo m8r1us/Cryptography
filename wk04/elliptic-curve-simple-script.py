d47ae137420d6b2576ef493f0bb7b7195bd0e1a0
Marius Elmiger <marius@elmigers.ch>
2020-03-07 20:29:54 +0100

    commit

--- wk04/elliptic-curve-simple-script.py
+++ wk04/elliptic-curve-simple-script.py
@@ -3,8 +3,10 @@
 # Super simple Elliptic Curve Presentation. No imported libraries, wrappers, nothing. # For educational purposes only
 # Below are the public specs for Bitcoin's curve - the secp256k1
 # Specs: www.secg.org/collateral/sec2_final.pdf
+# bitaddress: https://www.bitaddress.org/bitaddress.org-v3.3.0-SHA256-dec17c07685e1870960903d8f58090475b25af946fe95a734f88408cef4aa194.html
 
 import time
+steps = 1 #1=Shows the ADD and DUB steps / 0= don't show
 
 Pcurve = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 -1 # The proven prime
 N=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # Number of points in the field
@@ -45,25 +47,27 @@ def EccMultiply(xs,ys,Scalar): # Double & add. EC Multiplication, Not true multi
     if Scalar == 0 or Scalar >= N: raise Exception("Invalid Scalar/Private Key")
     ScalarBin = str(bin(Scalar))[2:]
     Qx,Qy=xs,ys
-    print ("Double or add based on 0 and 1")
-    print (bin(privKey))
-    print ("")
+    if steps==1:
+        print ("\tDouble or add based on 0 and 1")
+        print ("\tPrivat Key: ",bin(privKey))
+        print ("\t")
     for i in range (1, len(ScalarBin)): # This is invented EC multiplication.
         Qx,Qy=ECdouble(Qx,Qy) 
         TestScalarBin = ScalarBin[i]
-        if i < 10: # Only to show the first 10 steps
-            print ("DUB", Qx)
-            print ("\tRunning BIN",TestScalarBin)
+        if i < 10 and steps==1: # Only to show the first 10 steps
+            print ("\tDUB", Qx)
+            print ("\t\tRunning BIN",TestScalarBin)
         if TestScalarBin == "1":
             Qx,Qy=ECadd(Qx,Qy,xs,ys) 
-        if i < 10: # Only to show the first 10 steps
-            print ("ADD", Qx)
+        if i < 10 and steps==1: # Only to show the first 10 steps
+            print ("\tADD", Qx)
             time.sleep(2)
     return (Qx,Qy)
 
 print ("")
 print ("******* Public Key Generation *********")
 xPublicKey, yPublicKey = EccMultiply(Gx,Gy,privKey)
+print ("")
 print ("the private key (in base 10 format):")
 print (privKey) 
 print ("")
@@ -78,15 +82,16 @@ if yPublicKey % 2 == 1: # If the Y value for the Public Key is odd.
     print ("03"+str(hex(xPublicKey)[2:]).zfill(64))
 else: # Or else, if the Y value is even.
     print ("02"+str(hex(xPublicKey)[2:]).zfill(64))
-time.sleep(5)
+time.sleep(4)
 print ("")
 print ("******* Signature Generation *********")
 xRandSignPoint, yRandSignPoint = EccMultiply(Gx,Gy,RandNum)
 r = xRandSignPoint % N
+print ("")
 print ("r =", r)
 s = ((HashOfThingToSign + r*privKey)*(modinv(RandNum,N))) % N 
 print ("s =", s)
-
+time.sleep(4)
 print ("")
 print ("******* Signature Verification *********>>")
 w = modinv(s,N)
